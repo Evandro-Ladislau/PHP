9f66f1e7d87746f74419879ebfb34d98ad095abf
Evandro Ladislau <evandroladislau23@gmail.com>
2021-05-12 18:17:07 -0300

    'atualizacao'

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de inscrição</title>
+</head>
+<body>
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php" method="post">
+    <p>Your name: <Input type="text" name="nome" /></p>
+    <p>Your age: <Input type="text" name="idade" /></p>
+    <p><Input type="submit" /></p>
+</body>
+</html>
